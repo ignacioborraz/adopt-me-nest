06ee0fa8a4bb609628c123f1b1cd0a75198cfef9
ignacio.borraz <ignacio.borraz@koonolmexico.com>
2023-07-17 18:02:41 -0300

    links

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,3 +1,29 @@
 import Route from '@ember/routing/route';
 
-export default class AboutRoute extends Route {}
+let bookmarks = [
+  {
+    id: 1,
+    title: 'Microsoft',
+    link: 'https://www.microsoft.com/',
+    about:
+      'At Microsoft our mission and values are to help people and businesses throughout the world realize their full potential.',
+  },
+  {
+    id: 2,
+    title: 'Wikipedia',
+    link: 'https://en.wikipedia.org/wiki/Main_Page',
+    about: 'The Free Encyclopedia',
+  },
+  {
+    id: 3,
+    title: 'Google',
+    link: 'https://www.google.com/',
+    about: null,
+  },
+];
+
+export default class AboutRoute extends Route {
+  model() {
+    return bookmarks;
+  }
+}
