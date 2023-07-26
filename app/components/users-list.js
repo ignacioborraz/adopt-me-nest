c0479faee2020685363d8afd48e19734e9050024
ignacio.borraz <ignacio.borraz@koonolmexico.com>
2023-07-26 19:38:46 -0300

    components

--- app/components/users-list.js
+++ app/components/users-list.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+const UserListComponent = Ember.Component.extend({
+    imgClass: "avatar",
+    click() {
+        Ember.Logger.info("user-list was cliecked")
+        return false
+    }
 });
+
+UserListComponent.reopenClass({
+    positionalParams: ['avatarUrl','email']
+})
+
+export default UserListComponent
